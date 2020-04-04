9744a9c9928c5e960bc80fba564e2fef3127e7c4
Hans Hendrickx <hhlhendrickx@gmail.com>
2020-04-04 11:52:29 +0200

    Ch2 Postgresql implementation

--- postgresql_project/settings.py
+++ postgresql_project/settings.py
@@ -73,11 +73,15 @@ WSGI_APPLICATION = 'postgresql_project.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
+DATABASES = { 
+    'default': { 
+        'ENGINE': 'django.db.backends.postgresql', 
+        'NAME': 'wsvpostgres', 
+        'USER': 'postgres', 
+        'PASSWORD': 'admin123', 
+        'HOST': '', 
+        'PORT': 5432 
+    } 
 }
 
 
